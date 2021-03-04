002721484566b290de060e26a787aff65954beed
Nadezhda <nstitsyuk@andata.ru>
2021-03-04 22:36:26 +0300

    lesson 2: Добавили шаблон html

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)  # передает данный файл
@@ -7,13 +7,19 @@ app = Flask(__name__)  # передает данный файл
 # отслеживает определенный url адресс
 # главная страница
 @app.route('/')  # декоратор
+@app.route('/home')  # декоратор
 def index():
-    return 'It is index page'
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-    return 'It is about page'
+    return render_template('about.html')
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return f'It is user page: {name}, {id}'
 
 
 if __name__ == "__main__":
