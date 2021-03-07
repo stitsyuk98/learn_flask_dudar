1522eaedb15748664d7194e49c8278c0370ba710
Nadezhda <nstitsyuk@andata.ru>
2021-03-07 19:52:57 +0300

    lesson 3 Создана база данных Написана страница добавления статьи

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import  SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)  # передает данный файл
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'  # объявили базу данных
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
 
 
 # отслеживает определенный url адресс
@@ -17,9 +34,23 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return f'It is user page: {name}, {id}'
+@app.route('/create-article', methods=['GET', 'POST'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        return render_template('create-article.html')
 
 
 if __name__ == "__main__":
